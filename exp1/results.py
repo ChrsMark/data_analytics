0b6e575068aabfa385a11268d611d77e403ee07e
Chris Mark <chrismarkou92@gmail.com>
2018-04-14 10:51:36 +0000

    better graphs

--- exp1/results.py
+++ exp1/results.py
@@ -10,13 +10,13 @@ def generatePlot3D(filename):
     plot("set view 60,30")
     plot("set xlabel 'r'")
     plot("set ylabel 'm'")
-    plot("set zlabel 'mean_score'")
+    plot("set zlabel 'max_score'")
     plot("set title 'Different x-y measurement'")
     plot("set ztics offset 1")
-    plot("set zrange [0:20000]")
+    plot("set zrange [0:80000]")
     plot("set palette defined (0.001 'green', 0.01 'yellow', 0.9 'red')")
     plot("set pm3d")
-    plot.splot(Gnuplot.File('test_results_R_mean_score.txt', using=(1,2,3) ))#, with_="lines linecolor palette"))
+    plot.splot(Gnuplot.File('test_results_R_max_score.txt', using=(1,2,3) ))#, with_="lines linecolor palette"))
     return
 
-generatePlot3D("ex1_R_mean_score.png")
+generatePlot3D("ex1_R_max_score_z_big.png")
